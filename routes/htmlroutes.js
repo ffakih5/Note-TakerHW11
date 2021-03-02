718be72da5135d4c1cbbb2be5ccfe3674f9edbd7
Fatima Fakih <fatima.fakih@hotmail.com.au>
2021-03-02 22:17:11 +1100

    notes.html route, GET request function, user shown html file when request met.

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -1,3 +1,10 @@
 //path package
 const path = require('path');
 
+module.exports = (app) => {
+
+    app.get('/notes', (err,res)=>{
+        res.sendFile(path.join(__dirname, '..public/notes.html'));
+    });
+}
+
