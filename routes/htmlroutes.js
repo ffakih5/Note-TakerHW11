60c27ba98b266863d707fb4f7094d9f9f6226acb
Fatima Fakih <fatima.fakih@hotmail.com.au>
2021-03-02 22:28:36 +1100

    added default route to home/index page

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -3,14 +3,16 @@ const path = require('path');
 
 module.exports = (app) => {
 
-    app.get('/notes', (req,res)=>{
+    app.get('/notes', (req,res) => {
         res.sendFile(path.join(__dirname, '..public/notes.html'));
     });
 
-    app.get('/notes', (req,res)=>{
+    app.get('/notes', (req,res) => {
         res.sendFile(path.join(__dirname, '..public/index.html'));
     }); 
 
-    app.get ('*', (re))
-}
+    app.get ('*', (req,res) => {
+        res.sendFile(path.join(__dirname, '../public/notes.html'));
+    });
+};
 
