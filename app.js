b02f1f6b839982c828866054da25b882e8cff43d
Fatima Fakih <fatima.fakih@hotmail.com.au>
2021-03-06 15:01:46 +1100

    require routes api/html and app callback

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public')) 
 
+require('./routes/apiroutes') (app);
+require('./routes/htmlroutes') (app);
+
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
 
