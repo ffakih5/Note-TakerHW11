4ae158eabed7501a51e42377e8c8e4aa0f4c7389
Fatima Fakih <fatima.fakih@hotmail.com.au>
2021-03-06 22:24:25 +1100

    removing psuedo code and fixed line 15 on html route to index.html

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -12,7 +12,7 @@ module.exports = (app) => {
     }); 
 
     app.get ('*', (req,res) => {
-        res.sendFile(path.join(__dirname, '../public/notes.html'));
+        res.sendFile(path.join(__dirname, '../public/index.html'));
     });
 };
 
