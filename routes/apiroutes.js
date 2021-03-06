8136a674c9b84053470b1de86f7497c7410284e5
Fatima Fakih <fatima.fakih@hotmail.com.au>
2021-03-06 16:20:01 +1100

    app.get const dbInput and app.post const usernOTE

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -5,15 +5,17 @@ module.exports = (app) => {
     app.get('/api/notes', (req,res) => {
       fs.readFile('./db/db.json', (err, data) => {
         if (err) throw err;
-        
-
-      })
-
-        
-
-    })
+        dbInput = JSON.parse(data);
+        res.send(dbInput);
+      });
+    });
 
     app.post('/api/notes', (req, res) => {
+      const userNote = req.body;
+
+      fs.readFile('./db/db.json', (err, data) => {
+
+      })
         // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
         // It will do this by sending out the value "true" have a table
         // req.body is available since we're using the body parsing middleware
